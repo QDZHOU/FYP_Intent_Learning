07c217a11c4c30eb7099c06bf0dc7b70038d0423
Qidong Zhou <qidong.zhou21@imperial.ac.uk>
2025-01-18 16:22:00 +0000

    update:ellipsoid

--- H_estimation/ellipsoid.py
+++ H_estimation/ellipsoid.py
@@ -80,6 +80,75 @@ class ellipsoid_estimation_offline():
     def Return(self):
         pass
 
+class ellipsoid_estimation_online1():
+    def __init__(self, Param):
+        self.SV_Acc = Param["init_acc"]
+        self.N = Param["N"] # Reachability Prediction Length
+        self.T = Param["T"] # Sampling Interval
+        
+        A_init = np.array([[1, 0], [0, 1]])
+        b_init = np.array([[0], [0]]) 
+        c_init = b_init.T@b_init-1
+
+        self.nu = self.SV_Acc.shape[0]
+        self.A_pre, self.b_pre = self.opti_SDP_lemma(A_init,b_init,self.SV_Acc)
+
+    def ReachableSet(self,SV_Acc_new, SV_Pos_new, SV_Vel_new):
+        self.Ellipsoid(SV_Acc_new)
+        # occupancy projection
+
+    def opti_SDP_lemma(self, A_pre, b_pre,SV_Acc_new):
+        nu = self.nu
+
+        A = cp.Variable((nu, nu), PSD=True)
+        b = cp.Variable((nu, 1))
+        constraints = [A - 1e-8*np.eye(nu) >> 0]
+
+        constraints += [A.T@A - A_pre.T@A_pre >> 0]
+        constraints += [cp.norm(A@cp.reshape(SV_Acc_new, (2, 1),order='F') + b, 2) <= 1]
+        constraints += [cp.norm(A @ (-inv(A_pre)@b_pre) + b, 2) <= 1]
+        objective = cp.Maximize(cp.log_det(A))
+
+        problem = cp.Problem(objective, constraints)
+        problem.solve(solver = cp.MOSEK) # verbose=True
+
+        return A.value, b.value
+
+    def Ellipsoid(self, SV_Acc_new):
+        self.A_pre, self.b_pre = self.opti_SDP_lemma(self.A_pre,self.b_pre,SV_Acc_new)
+
+    def Plot_Ellipsoid(self):
+        A = self.A_pre
+        b = self.b_pre
+        u_range = np.linspace(-7, 7, 400)
+        v_range = np.linspace(-7, 7, 400)
+        u, v = np.meshgrid(u_range, v_range)
+
+        # Vectorize u and v to apply the transformation
+        uv = np.vstack([u.flatten(), v.flatten()]).T
+
+        # Apply the transformation A*u + b
+        transformed_uv = np.dot(uv, A.T) + b.T
+
+        # Compute the quadratic form ||A*u + b||^2
+        quad_form = np.sum(transformed_uv**2, axis=1)
+
+        # Reshape to match the grid shape
+        quad_form = quad_form.reshape(u.shape)
+
+        # Plot the 2D ellipsoid using contour plot
+        plt.contour(u, v, quad_form, levels=[1], colors='b')  # Level set at 1
+        #plt.scatter(random_vals[0,:i],random_vals[1,:i],marker='x',color = 'black')
+        plt.title("Ellipsoid Visualisation")
+        plt.xlabel('Long Acc')
+        plt.ylabel('Lat Acc')
+        plt.axis('equal')
+        plt.grid(True)
+        plt.show()
+
+    def Return(self):
+        pass
+
 class ellipsoid_estimation_online():
     def __init__(self, Param):
         self.SV_Acc = Param["init_acc"]
