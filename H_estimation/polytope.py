1d3d0e22b64f1dc6ba4d24257fc26db9e2624571
Qidong Zhou <qidong.zhou21@imperial.ac.uk>
2025-01-22 13:12:38 +0000

    feat:occuancy visualization

--- H_estimation/polytope.py
+++ H_estimation/polytope.py
@@ -168,32 +168,31 @@ class polytope_estimation_MH():
 
     def ReachableSet(self,SV_Acc_new, SV_Pos_new, SV_Vel_new):
         self.U_Hat_Poly = self.MH(SV_Acc_new)
-        #self.Plot_Polytope()
 
-        # A_SV = np.array([[1, self.T, 0, 0], [0, 1, 0, 0], [0, 0, 1, self.T], [0, 0, 0, 1]])
-        # B_SV = np.array([[0.5*self.T**2, 0], [self.T, 0], [0, 0.5*self.T**2], [0, self.T]])
-        # BU = B_SV*self.U_Hat_Poly
-        # Reachable_Set = list()
-        # Occupancy_SV = list()
-        # x_t = np.array([SV_Pos_new[0], SV_Vel_new[0], SV_Pos_new[1], SV_Vel_new[1]])
-        # Reachable_Set.append(x_t)
+        A_SV = np.array([[1, self.T, 0, 0], [0, 1, 0, 0], [0, 0, 1, self.T], [0, 0, 0, 1]])
+        B_SV = np.array([[0.5*self.T**2, 0], [self.T, 0], [0, 0.5*self.T**2], [0, self.T]])
+        BU = B_SV*self.U_Hat_Poly
+        Reachable_Set = list()
+        Occupancy_SV = list()
+        x_t = np.array([SV_Pos_new[0], SV_Vel_new[0], SV_Pos_new[1], SV_Vel_new[1]])
+        Reachable_Set.append(x_t)
 
-        # for t in range(1,self.N+1):
-        #     if t == 1:
-        #         reachable_set_t = (A_SV@Reachable_Set[t - 1] + BU) 
-        #     else:
-        #         reachable_set_t = (A_SV*Reachable_Set[t - 1] + BU) 
+        for t in range(1,self.N+1):
+            if t == 1:
+                reachable_set_t = (A_SV@Reachable_Set[t - 1] + BU) 
+            else:
+                reachable_set_t = (A_SV*Reachable_Set[t - 1] + BU) 
             
-        #     vertex = reachable_set_t.V
-        #     vertex_xy = np.delete(vertex, [1, 3], axis = 1)
-        #     occupancy_SV_t = Polytope(vertex_xy) 
-        #     occupancy_SV_t.minimize_V_rep( )
-        #     temp_poly   = occupancy_SV_t
+            vertex = reachable_set_t.V
+            vertex_xy = np.delete(vertex, [1, 3], axis = 1)
+            occupancy_SV_t = Polytope(vertex_xy) 
+            occupancy_SV_t.minimize_V_rep( )
+            temp_poly   = occupancy_SV_t
 
-        #     Occupancy_SV.append(temp_poly)
-        #     Reachable_Set.append(reachable_set_t)
+            Occupancy_SV.append(temp_poly)
+            Reachable_Set.append(reachable_set_t)
 
-        # return Occupancy_SV
+        return Occupancy_SV
 
 
     def Generate_Polytope(self, radius, num_vertices):
@@ -310,31 +309,123 @@ class polytope_estimation_OR():
     def ReachableSet(self,SV_Acc_new, SV_Pos_new, SV_Vel_new):
         self.U_Hat_Poly = self.OR(SV_Acc_new)
 
-        # A_SV = np.array([[1, self.T, 0, 0], [0, 1, 0, 0], [0, 0, 1, self.T], [0, 0, 0, 1]])
-        # B_SV = np.array([[0.5*self.T**2, 0], [self.T, 0], [0, 0.5*self.T**2], [0, self.T]])
-        # BU = B_SV*self.U_Hat_Poly
-        # Reachable_Set = list()
-        # Occupancy_SV = list()
-        # x_t = np.array([SV_Pos_new[0], SV_Vel_new[0], SV_Pos_new[1], SV_Vel_new[1]])
-        # Reachable_Set.append(x_t)
+        A_SV = np.array([[1, self.T, 0, 0], [0, 1, 0, 0], [0, 0, 1, self.T], [0, 0, 0, 1]])
+        B_SV = np.array([[0.5*self.T**2, 0], [self.T, 0], [0, 0.5*self.T**2], [0, self.T]])
+        BU = B_SV*self.U_Hat_Poly
+        Reachable_Set = list()
+        Occupancy_SV = list()
+        x_t = np.array([SV_Pos_new[0], SV_Vel_new[0], SV_Pos_new[1], SV_Vel_new[1]])
+        Reachable_Set.append(x_t)
 
-        # for t in range(1,self.N+1):
-        #     if t == 1:
-        #         reachable_set_t = (A_SV@Reachable_Set[t - 1] + BU) 
-        #     else:
-        #         reachable_set_t = (A_SV*Reachable_Set[t - 1] + BU) 
+        for t in range(1,self.N+1):
+            if t == 1:
+                reachable_set_t = (A_SV@Reachable_Set[t - 1] + BU) 
+            else:
+                reachable_set_t = (A_SV*Reachable_Set[t - 1] + BU) 
             
-        #     vertex = reachable_set_t.V
-        #     vertex_xy = np.delete(vertex, [1, 3], axis = 1)
-        #     occupancy_SV_t = Polytope(vertex_xy) 
-        #     occupancy_SV_t.minimize_V_rep( )
-        #     temp_poly   = occupancy_SV_t
+            vertex = reachable_set_t.V
+            vertex_xy = np.delete(vertex, [1, 3], axis = 1)
+            occupancy_SV_t = Polytope(vertex_xy) 
+            occupancy_SV_t.minimize_V_rep( )
+            temp_poly   = occupancy_SV_t
 
-        #     Occupancy_SV.append(temp_poly)
-        #     Reachable_Set.append(reachable_set_t)
+            Occupancy_SV.append(temp_poly)
+            Reachable_Set.append(reachable_set_t)
 
-        # return Occupancy_SV
+        return Occupancy_SV
 
+    def MPCFormulation(self):
+        d_min = self.d_min          #minimum distance
+        N = self.N                  #prediction length
+        DEV = self.DEV              #dimension
+        T = self.T                  #sampling interval
+        Q1 = self.Q1                #weighting matrix steering wheel angle
+        Q2 = self.Q2                #weighting matrix longitudinal jerk
+        Q3 = self.Q3                #weighting matrix long. velocity error
+        Q4 = self.Q4                #weighting matrix long. pos. error
+        Q5 = self.Q5                #weighting matrix lat. pos. error
+        Q6 = self.Q6                #weighting matrix heading. error
+        Q7 = self.Q7                #weighting matrix slack variable
+        A_road = self.A_road        #driving area.A
+        b_road = self.b_road        #driving area.B
+        v_low = self.v_low 
+        v_up = self.v_up 
+        acc_low = self.acc_low 
+        acc_up = self.acc_up 
+        delta_low = self.delta_low 
+        delta_up = self.delta_up
+
+        opti = ca.Opti( )
+        X = opti.variable(DEV, N + 1)
+        U = opti.variable(2, N)
+        delta = U[0, :]
+        eta   = U[1, :]
+        lam = opti.variable(4, N)
+        s   = opti.variable(N, 1)
+
+        G = opti.parameter(4, 2*N)
+        g = opti.parameter(4, N)
+        Initial = opti.parameter(DEV, 1)
+        v_ref = opti.parameter( )
+        x_ref = opti.parameter( )
+        y_ref = opti.parameter( )
+        phi_ref = opti.parameter( )
+        
+        opti.subject_to(X[:, 0] == Initial)
+        for k in range(N):
+            k1 = self.vehicle_model(X[:, k], delta[k], eta[k])
+            k2 = self.vehicle_model(X[:, k] + T/2*k1, delta[k], eta[k])
+            k3 = self.vehicle_model(X[:, k] + T/2*k2, delta[k], eta[k])
+            k4 = self.vehicle_model(X[:, k] + T*k3, delta[k], eta[k])
+            x_next = X[:, k] + T/6 * (k1 + 2*k2 + 2*k3 + k4)
+            opti.subject_to(X[:, k + 1] == x_next) 
+        x = X[0, 1::]
+        y = X[1, 1::]
+        phi = X[2, 1::]
+        v = X[3, 1::]
+        a = X[4, 1::]
+        phi_error = phi[-1] - phi_ref
+        v_error = v[-1] - v_ref # longitudinal speed error
+        x_error = x[-1] - x_ref # longitudinal position error
+        y_error = y[-1] - y_ref # lateral position error
+        # collision-avoidance
+        for k in range(N):
+            p_point = X[0:2, k + 1]
+            
+            G_point = G[:, 2*k:2*k + 2]
+            g_point = g[:, k]
+            temp = G_point.T@lam[:, k]
+            opti.subject_to((G_point@p_point - g_point).T@lam[:, k] >= d_min - s[k])
+            opti.subject_to(temp[0]**2 + temp[1]**2 == 1)
+            opti.subject_to(0 <= lam[:, k])
+            opti.subject_to(A_road@p_point <= b_road)
+
+        opti.subject_to(opti.bounded(0, s, d_min))
+        opti.subject_to(opti.bounded(-v_up, v, v_up))
+        opti.subject_to(opti.bounded(acc_low, a, acc_up))
+        opti.subject_to(opti.bounded(delta_low, delta, delta_up))
+        
+        J = delta@Q1@delta.T + eta@Q2@eta.T + Q3*v_error@v_error.T + Q4*x_error@x_error.T + Q5*y_error@y_error.T + Q6*phi_error@phi_error.T + Q7*s.T@s
+        opti.minimize(J)
+        
+        opts = {"ipopt.print_level": 0, "ipopt.linear_solver": "ma57", "print_time": False}
+        opti.solver('ipopt', opts)
+
+        return opti.to_function('g', [G, g, Initial, v_ref, x_ref, y_ref, phi_ref], [X, U, J, s])
+    
+    def  vehicle_model(self, w, delta, eta):
+
+        l_f = self.l_f
+        l_r = self.l_r
+        
+        beta = np.arctan(l_r/(l_f + l_r)*np.tan(delta))
+        x_dot   = w[3]*np.cos(w[2] + beta) 
+        y_dot   = w[3]*np.sin(w[2] + beta)
+        phi_dot = w[3]/(l_r)*np.sin(beta)
+        v_dot = w[4]
+        a_dot = eta
+        
+        return ca.vertcat(x_dot, y_dot, phi_dot, v_dot, a_dot)
 
     def Generate_Polytope(self, radius, num_vertices):
         if num_vertices != 4:
